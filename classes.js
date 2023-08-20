ac38aef539887679365f2fc7de6928830a6ea00a
Maÿ (Mio) <133236637+mio-exe@users.noreply.github.com>
2023-08-20 11:48:24 +0200

    Add files via upload

--- classes.js
+++ classes.js
@@ -1,5 +1,5 @@
 class Sprite {
-    constructor({position, image, frames = {max : 1, hold: 10}, sprites, animate = false, isEnemy = false}) {
+    constructor({position, image, frames = {max: 1, hold: 10}, sprites, animate = false, rotation = 0,}) {
         this.position = position;
         this.image = image;
         this.frames = {...frames, val: 0, elapsed: 0};
@@ -10,11 +10,14 @@ class Sprite {
         this.animate = animate;
         this.sprites = sprites;
         this.opacity = 1;
-        this.health = 100;
-        this.isEnemy =  isEnemy;
+        this.rotation = rotation;
     }
+
     draw() {
         c.save();
+        c.translate(this.position.x + this.width / 2, this.position.y + this.height / 2);
+        c.rotate(this.rotation);
+        c.translate(-this.position.x - this.width / 2, -this.position.y - this.height / 2);
         c.globalAlpha = this.opacity;
         c.drawImage(this.image, this.frames.val * this.width, 0,
             this.image.width / this.frames.max,
@@ -25,57 +28,124 @@ class Sprite {
             this.image.height);
         c.restore();
 
-        if (!this.animate){return;}
+        if (!this.animate) {
+            return;
+        }
 
-            if (this.frames.max > 1) {
-                this.frames.elapsed++;
+        if (this.frames.max > 1) {
+            this.frames.elapsed++;
+        }
+
+        if (this.frames.elapsed % this.frames.hold === 0) {
+            if (this.frames.val < this.frames.max - 1) {
+                this.frames.val++;
+            } else {
+                this.frames.val = 0;
             }
-
-            if (this.frames.elapsed % this.frames.hold === 0) {
-                if (this.frames.val < this.frames.max - 1) {
-                    this.frames.val++;
-                } else {
-                    this.frames.val = 0;
-                }
         }
     }
-    attack({attack, recipient}){
-        const tl = gsap.timeline();
-        this.health -= attack.damage;
-        
-        let movementDistance = 20;
-        if (this.isEnemy) movementDistance = -20;
+}
+
+class Monster extends Sprite {
+    constructor({position, image, frames = {max: 1, hold: 10}, sprites, animate = false, rotation = 0, isEnemy = false, name, attacks}) {
+        super({
+            position, image, frames, sprites, animate, rotation
+        });
+        this.health = 100;
+        this.isEnemy = isEnemy;
+        this.name = name;
+        this.attacks = attacks;
+    }
+    attack({attack, recipient, renderedSprites}) {
+        document.querySelector('#dialogueBox').style.display = 'block';
+        document.querySelector('#dialogueBox').innerHTML = this.name + ' used ' + attack.name;
 
         let healthBar = '#enemyHealthBar';
         if (this.isEnemy) healthBar = '#playerHealthBar';
+        let rotation = 1;
+        if (this.isEnemy) rotation = -2.2;
+        this.health -= attack.damage;
 
-        tl.to(this.position, {
-            x: this.position.x -movementDistance,
-        }).to(this.position, {
-            x: this.position.x +movementDistance * 2,
-            duration: 0.1,
-            onComplete: () => {
-                gsap.to(healthBar, {
-                    width: this.health + '%',
+        switch (attack.name) {
+            case 'Fireball':
+                const fireballImage = new Image();
+                fireballImage.src = './img/fireball.png'
+                const fireball = new Sprite({
+                    position: {
+                        x: this.position.x,
+                        y: this.position.y,
+                    },
+                    image: fireballImage,
+                    frames: {
+                        max: 4,
+                        hold: 10,
+                    },
+                    animate: true,
+                    rotation,
                 })
-                gsap.to(recipient.position, {
-                    x: recipient.position.x + 10,
-                    yoyo: true,
-                    repeat: 5,
-                    duration: 0.08,
+                renderedSprites.splice(1, 0,fireball);
+
+                gsap.to(fireball.position, {
+                    x: recipient.position.x,
+                    y: recipient.position.y,
+                    onComplete: () => {
+                        gsap.to(healthBar, {
+                            width: this.health + '%',
+                        })
+                        gsap.to(recipient.position, {
+                            x: recipient.position.x + 10,
+                            yoyo: true,
+                            repeat: 5,
+                            duration: 0.08,
+                        })
+                        gsap.to(recipient, {
+                            opacity: 0,
+                            repeat: 5,
+                            yoyo: true,
+                            duration: 0.08,
+                        })
+                        renderedSprites.splice(1, 1)
+                    }
                 })
-                gsap.to(recipient, {
-                    opacity: 0,
-                    repeat: 5,
-                    yoyo: true,
-                    duration: 0.08,
+                break;
+
+            case'Tackle':
+                const tl = gsap.timeline();
+
+                let movementDistance = 20;
+                if (this.isEnemy) movementDistance = -20;
+
+
+                tl.to(this.position, {
+                    x: this.position.x - movementDistance,
+                }).to(this.position, {
+                    x: this.position.x + movementDistance * 2,
+                    duration: 0.1,
+                    onComplete: () => {
+                        gsap.to(healthBar, {
+                            width: this.health + '%',
+                        })
+                        gsap.to(recipient.position, {
+                            x: recipient.position.x + 10,
+                            yoyo: true,
+                            repeat: 5,
+                            duration: 0.08,
+                        })
+                        gsap.to(recipient, {
+                            opacity: 0,
+                            repeat: 5,
+                            yoyo: true,
+                            duration: 0.08,
+                        })
+                    }
                 })
-            }
-        })
-            .to(this.position, {
-            x: this.position.x,
-        })
-    };
+                    .to(this.position, {
+                        x: this.position.x,
+                    })
+
+                break
+        }
+    }
 }
 
 class Boundary {
